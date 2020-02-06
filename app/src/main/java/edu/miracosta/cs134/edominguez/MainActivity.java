ac13c7644bd0cd733ef3045618c67b81b0d9f7f0
Emmanuel <Emannyd1@gmail.com>
2020-02-05 23:31:43 -0800

    finished product, opens and tap to count!

--- app/src/main/java/edu/miracosta/cs134/edominguez/MainActivity.java
+++ app/src/main/java/edu/miracosta/cs134/edominguez/MainActivity.java
@@ -2,13 +2,35 @@ package edu.miracosta.cs134.edominguez;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private Button tapMeButton;
+    private TextView countTextView;
+    private int count = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Manny was HERE!!
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Manny was HERE!!
+        //wire it up
+
+        tapMeButton = findViewById(R.id.tapMeButton);
+        countTextView = findViewById(R.id.countTextView);
+
+
+
+
+    }
+
+    public void addCount(View v)
+    {
+        countTextView.setText(Integer.toString(++count));
     }
 }
